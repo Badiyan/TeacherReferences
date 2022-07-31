d23ca2498d71c6bec7392f88ea78ae8e8e308387
Badian <43342777+Badiyan@users.noreply.github.com>
2022-07-31 21:21:58 +0300

    Add files via upload

---
+++ 2lesson_FirstAppDemo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _20220717_FirstAppDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region -=- Hello! -=-
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("*******************");
+            Console.WriteLine("*******************");
+            Console.WriteLine("*******************");
+            Console.WriteLine("*******************");     
+
+            #endregion
+
+            Console.ReadKey();    // одидание нажатия любой клавиши
+        }
+    }
+}
